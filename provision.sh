6a70b353f7f2cd39860a5f03b6c8cc05f306d346
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-07-20 23:46:41 +0900

    added provision.sh

---
+++ provision.sh
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env bash
+
+sudo yum -y update
+sudo yum -y install httpd
+sudo service httpd start
+sudo chkconfig httpd on
+sudo service iptables stop
+sudo chkconfig iptables off
+sudo yum -y update
+sudo ln -fs /vagrant /var/www/html
